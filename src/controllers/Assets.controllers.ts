8295d114b70e8b8983d711dd6b6924a92d84382c
Bruno Vieira <vieira.es@gmail.com>
2023-05-27 17:21:13 -0300

    :adhesive_bandage: Fix wallet routes

--- src/controllers/Assets.controllers.ts
+++ src/controllers/Assets.controllers.ts
@@ -4,47 +4,34 @@ import { prisma } from '../libs/db'
 import { v4 as uuidv4 } from 'uuid';
 import { IAsset } from '../interfaces/IAsset';
 import { IWallet } from '../interfaces/IWallet';
-
-
-enum AssetEnum {
-    BTC = "BTC",
-    BRL = 'BRL',
-    USD = 'USD'
-}
+import { AssetEnum } from '../enums/asset.enum';
 
 const router = express.Router()
 
 router.post('/', async (req: Request, res: Response) => {
     try {
-        const { ticker }: { ticker: string } = req.body
-        const { walletId }: { walletId: string } = req.body
-        
+        const { ticker, walletId }: { walletId: string, ticker: string } = req.body
+        const { isRootAsset }: { isRootAsset: boolean } = req.body
+
         let handleError = (res: Response, statusCode: number, message: string) => {
             res.status(statusCode).send({ message })
             console.error(message)
         }
-        if (!ticker || !walletId) {
-            handleError(res, 400, "walletId or ticker is required")
-            return
-        }
-        if (!(ticker in AssetEnum)) {
-            handleError(res, 400, "ticker is not valid")
-            return
-        }
-        const walletReturn: IWallet | null = await prisma.wallet.findFirst({
+        // !isRootAsset ? handleError(res, 400, "isRootAsset is required") : undefined
+        !ticker || !walletId ? handleError(res, 400, "walletId or ticker is required") : undefined
+        !(ticker in AssetEnum) ? handleError(res, 400, "ticker is not valid") : undefined
+
+        const walletReturn: IWallet | null = await prisma.wallet.findUnique({
             where: {
                 id: walletId
             }
         })
+        !walletReturn ? handleError(res, 404, "wallet not found") : undefined
+
         if ((walletReturn?.assets as string[])?.includes(ticker)) {
             handleError(res, 412, "asset already exists on the wallet")
             return
         }
-        if (!walletReturn) {
-            handleError(res, 404, "cannot create assset. walletId not found")
-            return
-        }
-
 
         const assetCreated: IAsset = await prisma.asset.create({
             data: {
@@ -52,12 +39,10 @@ router.post('/', async (req: Request, res: Response) => {
                 ticker: ticker,
                 balance: 0.00,
                 createdAt: new Date().toISOString(),
-                walletId: walletReturn.id as string,
-                rootAsset: false
+                walletId: walletReturn ? walletReturn.id as string | any : undefined,
+                rootAsset: isRootAsset
             }
         })
-
-
         res.status(201).send(assetCreated)
     } catch (error) {
         console.error('Error', error)
--- src/controllers/Wallets.controllers.ts
+++ src/controllers/Wallets.controllers.ts
@@ -5,32 +5,33 @@ import { v4 as uuidv4 } from 'uuid';
 import { IWallet } from '../interfaces/IWallet';
 import { IUser } from '../interfaces/IUser';
 import { IAsset } from '../interfaces/IAsset';
+import { AssetEnum } from '../enums/asset.enum';
 
 const router = express.Router()
 
 
 router.post('/', async (req: Request, res: Response) => {
     try {
-        const { userId }: { userId: string } = req.body
+        const { userId, asset }: { asset: string, userId: string } = req.body
+        const { isRootAsset }: { isRootAsset: boolean } = req.body
+
         let handleError = (res: Response, statusCode: number, message: string) => {
             res.status(statusCode).send({ message })
             console.error(message)
         }
-        if (!userId) {
-            handleError(res, 400, "user reqeuired")
-            return
-        }
-        const returnUser: IUser | null = await prisma.user.findUnique({
+        !userId ? handleError(res, 400, "user is requised") : undefined
+        !asset ? handleError(res, 400, "asset is required") : undefined
+        !isRootAsset ? handleError(res, 400, "rootAsset is required") : undefined
+        !(asset in AssetEnum) ? handleError(res, 400, "Asset not permitted") : undefined
+
+        let returnUser: IUser | null = await prisma.user.findFirst({
             where: {
                 id: userId
             }
         })
-        if (!returnUser) {
-            handleError(res, 400, 'User not found')
-            return
-        }
+        !returnUser ? handleError(res, 400, 'User not found') : undefined
 
-        let newWalltId = uuidv4() as string
+        let newWalletId = uuidv4() as string
         let newAssetId = uuidv4() as string
 
         // const assetRoot: IAsset = await prisma.asset.create({
@@ -43,37 +44,44 @@ router.post('/', async (req: Request, res: Response) => {
         //         rootAsset: true
         //     }
         // })
-        const findAsset = await prisma.asset.findFirst({
-            where: {
-                id: newAssetId
-            }
-        })
+        // const findAsset = await prisma.asset.findUniqueOrThrow({
+        //     where: {
+        //         id: newAssetId
+        //     }
+        // })
         // if (!findAsset) {
         //     handleError(res, 400, 'Error creating asset')
         //     return
         // }
-        //TODO: continuar com a criacao de wallet
         const newWallet: IWallet = await prisma.wallet.create({
             data: {
-                id: newWalltId,
+                id: newWalletId,
                 createdAt: new Date().toISOString(),
                 userId: userId,
                 assets: {
                     create: {
                         id: newAssetId,
-                        ticker: "BRL",
+                        ticker: asset,
                         balance: 0.00,
                         createdAt: new Date().toISOString(),
-                        rootAsset: true
+                        rootAsset: isRootAsset
                     }
                 }
             }
         })
 
-        res.status(201).send({
-            "message": "Wallet created",
-            ...newWallet
+        let assetReturn: IAsset | null = await prisma.asset.findUnique({
+            where: {
+                id: newAssetId
+            }
         })
+        !assetReturn ? handleError(res, 404, 'Error to find the new asset created') : undefined
+
+        res.status(201).send({
+            ...await newWallet,
+            assets: [{ ...await assetReturn }]
+        })
+
     } catch (error) {
         console.error('Error', error)
         res.status(400).send({
